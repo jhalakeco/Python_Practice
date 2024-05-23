264ad23f17c773a847162629f6fbcb73b5321052
Jhalak Gope <98821548+jhalakeco@users.noreply.github.com>
2024-05-23 03:26:07 +0200

    Add files via upload

---
+++ booleans.py
@@ -0,0 +1,14 @@
+age = 20
+
+is_over_age = age >= 18
+is_under_age = age < 18
+
+print(is_over_age)
+print(is_under_age)
+
+
+# A new program to work with boolean
+my_num = 5
+user_num = int(input("Enter your number: "))
+match_num = my_num == user_num
+print(f"Your assumed number is correct: {match_num}")
---
+++ logical_operators.py
@@ -0,0 +1,14 @@
+# OR AND [Logical operators] 
+
+# AND case
+age = 20
+age_comp = age > 15 and age < 16 # both of the logics must be True in order to get output as True
+age_comp
+
+# OR case
+age_2 = 30
+age_comp_2 = age_2 > 20 or age_2 < 25 # one of the logics must be True in order to get the output as True
+age_comp_2
+
+
+# bool function
